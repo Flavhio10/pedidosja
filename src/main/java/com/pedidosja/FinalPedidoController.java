4d15a1495b9bca80ad514cf27be69e3dc1518d96
Flavio Santos <flavhio10@gmail.com>
2022-12-14 05:57:09 -0300

    Tela de endereço

---
+++ src/main/java/com/pedidosja/FinalPedidoController.java
@@ -0,0 +1,13 @@
+package com.pedidosja;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FinalPedidoController {
+    
+    @GetMapping("/finalizarpedido")
+    public String finalPedido(){
+        return "/finalizarpedido";
+    }
+}
